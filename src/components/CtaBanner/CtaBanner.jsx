0dbe31eec8bb10ea8e521644c7cbea1379486f61
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-02-23 17:11:28 +0530

    Update CtaBanner.jsx

--- src/components/CtaBanner/CtaBanner.jsx
+++ src/components/CtaBanner/CtaBanner.jsx
@@ -7,7 +7,7 @@ function CtaBanner() {
         <div className="container page-padding py-[70px] text-white">
           <h2 className="text-[36px] font-bold">Need a Fitness Trainer?</h2>
           <h3 className="text-[28px] font-bold mb-20">
-            <span className="text-[#FF0336]">Call:</span> +123-456789
+            <span className="text-[#FF0336]">Call:</span> +91 - 9988776655
           </h3>
           <MainButton
             color={`text-white`}
