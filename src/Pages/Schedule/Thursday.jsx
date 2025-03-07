40b180904dab638d7761ac22a5bf79239e37358e
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-03-07 09:02:17 +0530

    Update Thursday.jsx

--- src/Pages/Schedule/Thursday.jsx
+++ src/Pages/Schedule/Thursday.jsx
@@ -79,10 +79,19 @@ function Thursday() {
               <p className="text-[14px] font-medium text-[#a0a0a0]">Trainer</p>
               <p className="text-[18px] text-black font-bold mt-3">David Vila</p>
             </li>
-            <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full text-right min800:text-center">
+{/*             <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full text-right min800:text-center">
               <button className="text-white text-[15px] font-medium bg-[#555] py-[10px] px-[20px] rounded-[30px] mt-3 hover:bg-[#ff0336] ease-in duration-200 ">
                 Join Now
               </button>
+            </li> */}
+                        <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full  min800:justify-center items-center flex justify-end">
+              <Link
+                onClick={() => window.top(0, 0)}
+                to="/contact"
+                className="text-white text-[15px] font-medium bg-[#555] py-[10px] px-[20px] rounded-[30px] hover:bg-[#ff0336] ease-in duration-200  "
+              >
+                Join Now
+              </Link>
             </li>
           </ul>
           {/* row */}
@@ -102,10 +111,19 @@ function Thursday() {
               <p className="text-[14px] font-medium text-[#a0a0a0]">Trainer</p>
               <p className="text-[18px] text-black font-bold mt-3">John Weights</p>
             </li>
-            <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full text-right min800:text-center">
+{/*             <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full text-right min800:text-center">
               <button className="text-white text-[15px] font-medium bg-[#555] py-[10px] px-[20px] rounded-[30px] mt-3 hover:bg-[#ff0336] ease-in duration-200 ">
                 Join Now
               </button>
+            </li> */}
+                        <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full  min800:justify-center items-center flex justify-end">
+              <Link
+                onClick={() => window.top(0, 0)}
+                to="/contact"
+                className="text-white text-[15px] font-medium bg-[#555] py-[10px] px-[20px] rounded-[30px] hover:bg-[#ff0336] ease-in duration-200  "
+              >
+                Join Now
+              </Link>
             </li>
           </ul>
           {/* row */}
@@ -125,10 +143,19 @@ function Thursday() {
               <p className="text-[14px] font-medium text-[#a0a0a0]">Trainer</p>
               <p className="text-[18px] text-black font-bold mt-3">Junifor Jonas</p>
             </li>
-            <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full text-right min800:text-center">
+{/*             <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full text-right min800:text-center">
               <button className="text-white text-[15px] font-medium bg-[#555] py-[10px] px-[20px] rounded-[30px] mt-3 hover:bg-[#ff0336] ease-in duration-200 ">
                 Join Now
               </button>
+            </li> */}
+                        <li className="py-[25px] px-[55px] bg-[#f2f2f2] w-full  min800:justify-center items-center flex justify-end">
+              <Link
+                onClick={() => window.top(0, 0)}
+                to="/contact"
+                className="text-white text-[15px] font-medium bg-[#555] py-[10px] px-[20px] rounded-[30px] hover:bg-[#ff0336] ease-in duration-200  "
+              >
+                Join Now
+              </Link>
             </li>
           </ul>
           S
