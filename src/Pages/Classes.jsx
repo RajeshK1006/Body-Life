ce702aa1ea4958f0fdd75d0f0944d6d766d7978f
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-03-20 22:17:43 +0530

    Update Classes.jsx

--- src/Pages/Classes.jsx
+++ src/Pages/Classes.jsx
@@ -23,16 +23,16 @@ function Classes() {
           }`}
         >
           <ClassesBox bgImg="cycling-bg" title="Cycling" trainer="            " date="     " />
-          <ClassesBox bgImg="meditaion-bg" title="Meditation" trainer="Maria Mich" date="Fri: 1:00 pm" />
+          <ClassesBox bgImg="meditaion-bg" title="Meditation" trainer="" date="" />
           {/* <ClassesBox bgImg="mma-bg" title="Boxing" trainer="John Flex" date="Tue: 4:00 pm" /> */}
           {/* <ClassesBox bgImg="karate-bg" title="Karate" trainer="David Rich" date="Sat: 9:00 am" /> */}
-          <ClassesBox bgImg="powerlift-bg" title="Power Lifting" trainer="Larry Wheels" date="Mon: 8:00 pm" />
-          <ClassesBox bgImg="workout-bg" title="Workout" trainer="Shawn Ray" date="Sun: 10:00 am" />
-          <ClassesBox bgImg="crossfit-bg" title="Crossfit" trainer="Jenifer Alex" date="Wen: 9:00 pm" />
+          <ClassesBox bgImg="powerlift-bg" title="Power Lifting" trainer="" date="" />
+          <ClassesBox bgImg="workout-bg" title="Workout" trainer="" date="" />
+          <ClassesBox bgImg="crossfit-bg" title="Crossfit" trainer="" date="" />
           {/* <ClassesBox bgImg="running-bg" title="Running" trainer="Zinia Zessy" date="Fri: 6:00 am" /> */}
-          <ClassesBox bgImg="bodybuilding-bg" title="Body Building" trainer="Jake Paul" date="Mon: 8:00 pm" />
-          <ClassesBox bgImg="yoga-bg" title="Yoga" trainer="Marta Mich" date="Wen: 8:00 am" />
-          <ClassesBox bgImg="fitness-bg" title="Fitness" trainer="Mia Williams" date="Sun: 5:00 pm" />
+          <ClassesBox bgImg="bodybuilding-bg" title="Body Building" trainer="" date="" />
+          <ClassesBox bgImg="yoga-bg" title="Yoga" trainer="" date="" />
+          <ClassesBox bgImg="fitness-bg" title="Fitness" trainer="" date="" />
         </div>
 
         {/* boxes 2*/}
